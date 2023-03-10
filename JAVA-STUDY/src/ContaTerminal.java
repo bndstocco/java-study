5171327d6630d15b295e4a5b8d9018a885b508ba
bndstocco <bndst77@gmail.com>
2023-03-10 17:00:51 -0300

    att

--- JAVA-STUDY/src/ContaTerminal.java
+++ JAVA-STUDY/src/ContaTerminal.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Helo, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o seu nome: ");
+        String nome = scanner.next();
+        System.out.print("Digite o seu cpf: ");
+        String cpf = scanner.next();
+        System.out.println("Conta criada com sucesso. Seja bem vindo, " + nome + "!");
     }
 }
